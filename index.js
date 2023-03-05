bbe4b9640379566a42416f50982385734b6e6d3b
Claire Ingram <mail.claireingram+skillsfl@gmail.com>
2023-03-05 16:17:51 +0000

    all intern tests pass

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
+const Intern = require ("./lib/Intern");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
@@ -19,3 +20,4 @@ const Engineer = require("./lib/Engineer");
 // for testing before prompts: 
 const employeeTest = new Employee("Claire", 22545, "employee@email.com");
 const engineerTest = new Engineer("Harry", 666, "engineer@email.com", "HarrySmith");
+const internTest = new Intern("Abigail", 5569, "intern@email.com", "Park Hall");
